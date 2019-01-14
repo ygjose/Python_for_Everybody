e9b07e1f899dfab5dcc8b8ec9ef6a810d4d7b575
Gang <37382523+ygjose@users.noreply.github.com>
2019-01-15 00:14:23 +0100

    Create Assignment_7.2.py

---
+++ Assignment_7.2.py
@@ -0,0 +1,17 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+total = 0.0
+for line in fh:
+    if line.startswith('X-DSPAM-Confidence:'):
+        count = count +1
+        value_starts = line.find(':')
+        values = line[value_starts+1: ]
+        fvalues = float(values)
+        total = total + fvalues
+        average = total/count
+    if not line.startswith("X-DSPAM-Confidence:") : 
+        continue
+   
+print ('Average spam confidence:', average)
