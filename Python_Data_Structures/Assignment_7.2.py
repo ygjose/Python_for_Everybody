79b7d947158f3a8ead47f3d9accd4bb6c91bdf43
Gang <37382523+ygjose@users.noreply.github.com>
2019-01-16 01:29:51 +0100

    Update Assignment_7.2.py

--- Python_Data_Structures/Assignment_7.2.py
+++ Python_Data_Structures/Assignment_7.2.py
@@ -13,3 +13,21 @@ for line in fh:
         average = total/count
    
 print ('Average spam confidence:', average)
+
+
+# Option 2
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+total = 0.0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : 
+            continue
+    count = count + 1
+    value_starts = line.find(':')
+    values = line[value_starts+1: ]
+    fvalues = float(values)
+    total = total + fvalues
+    average = total/count
+        
+print ('Average spam confidence:', average)
