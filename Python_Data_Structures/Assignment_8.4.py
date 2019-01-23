1326551006a2cf4ba69c47ab73e194007d1e07f8
Gang <37382523+ygjose@users.noreply.github.com>
2019-01-23 01:04:45 +0100

    Create Assignment_8.4.py

---
+++ Python_Data_Structures/Assignment_8.4.py
@@ -0,0 +1,14 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+# Open the file romeo.txt and read it line by line.
+for line in fh:
+    # For each line, split the line into a list of words using the split() method
+    for words in line.split():
+        # For each word on each line check to see if the word is already in the list
+        if words not in lst:
+            # if not append it to the list
+            lst.append(words)
+# sort and print the resulting words in alphabetical order
+lst.sort()   
+print(lst)
