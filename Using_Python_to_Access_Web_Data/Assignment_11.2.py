fa175d5c3ccc75a4c300d25a5b7b5f08cb34778a
Gang <37382523+ygjose@users.noreply.github.com>
2019-04-16 01:04:52 +0200

    Update Assignment_11.2.py

--- Using_Python_to_Access_Web_Data/Assignment_11.2.py
+++ Using_Python_to_Access_Web_Data/Assignment_11.2.py
@@ -12,3 +12,19 @@ for number in numlist:
     num = int(number)
     newlist.append(num)
 print('Sum: ', sum(newlist))
+
+
+# I found a nicer and shorter code by other people
+
+import re
+ihand=input('Enter your text: ')
+fhand=open(ihand)
+numlist=list()
+for line in fhand:
+    line=line.rstrip()
+    numbers=re.findall('[0-9]+', line)
+    for num in numbers:
+        num = int(num)
+        numlist.append(num)
+        
+print('Sum: ', sum(numlist))
